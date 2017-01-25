bbecdb9097a34c968201fc38a76963b932e7155a
ubiquity7 <ilisowski7@gmail.com>
2017-01-25 23:26:06 +0100

    Poprawa.

--- iOS-task-3.swift
+++ iOS-task-3.swift
@@ -1,44 +1,54 @@
 import Foundation
 let input = "2x3x4 1x2x3 2x2x2"
-let packages = input.components(separatedBy: " ")
-var sum1 = 0
-var sum2 = 0
-for package in packages {
-	let wsp = package.components(separatedBy: "x")
+if(!input.isEmpty) { // warunek, dane musza nie byc puste
 	
-	// obliczanie powierzchni papieru do pakowania
-	let x:Int! = Int(wsp[0]); // wysokosc
-	let y:Int! = Int(wsp[1]); // szerokosc
-	let z:Int! = Int(wsp[2]); // dlugosc
+	print("Dane wejsciowe nie sa puste.")
+	print("Dane wejsciowe to: \(input)")
+	let packages = input.components(separatedBy: " ")
+	var sum1 = 0
+	var sum2 = 0
+	for package in packages {
+		let wsp = package.components(separatedBy: "x")
 	
-	let p1 = x * y; // pole 1 sciany
-	let p2 = x * z; // pole 2 sciany
-	let p3 = y * z; // pole 3 sciany
+		// obliczanie powierzchni papieru do pakowania
+		let x:Int! = Int(wsp[0]); // wysokosc
+		let y:Int! = Int(wsp[1]); // szerokosc
+		let z:Int! = Int(wsp[2]); // dlugosc
 	
-	// pole najmniejszej sciany
-	let smallest = min(p1, p2, p3);
+		let p1 = x * y; // pole 1 sciany
+		let p2 = x * z; // pole 2 sciany
+		let p3 = y * z; // pole 3 sciany
 	
-	// powierzchnia
-	let p = 2*p1 + 2*p2 + 2*p3 + smallest;
+		// pole najmniejszej sciany
+		let smallest = min(p1, p2, p3);
 	
-	// calkowita powierzchnia
-	sum1 = sum1 + p;
+		// powierzchnia
+		let p = 2*p1 + 2*p2 + 2*p3 + smallest;
 	
-	// obliczanie dlugosci sznurka do obwiazania
-	let obw1 = 2*z + 2*y;
-	let obw2 = 2*x + 2*y;
+		// calkowita powierzchnia
+		sum1 = sum1 + p;
 	
-	// najmniejszy obwod
-	let shortest = min(obw1, obw2);
+		// obliczanie dlugosci sznurka do obwiazania
+		let obw1 = 2*z + 2*y;
+		let obw2 = 2*x + 2*y;
+		let obw3 = 2*x + 2*z;
 	
-	// dlugosc sznurka
-	let s = x*y*z + shortest;
+		// najmniejszy obwod
+		let shortest = min(obw1, obw2, obw3);
 	
-	// calkowita dlugosc sznurka
-	sum2 = sum2 + s;
+		// dlugosc sznurka
+		let s = x*y*z + shortest;
+	
+		// calkowita dlugosc sznurka
+		sum2 = sum2 + s;
+		
+	}
+	
+	print("Suma powierzchni papieru potrzebnego do pakowania: \(sum1)")
+
+	print("Suma dlugosc sznurka potrzebnego do zapakowania: \(sum2)")
 	
 }
-
-print("Suma powierzchni papieru potrzebnego do pakowania: \(sum1)")
-
-print("Suma dlugosc sznurka potrzebnego do zapakowania: \(sum2)")
+else { // jesli dane sa puste, wypisujemy odpowiedni komunikat
+	print("Dane wejsciowe sa puste. Popraw je.");
+}
